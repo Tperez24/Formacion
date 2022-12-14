4da0446b2b6eacf32312a4110f1f2e4839eb646c
Toni <tperez@ggtech.es>
2022-12-14 16:15:09 +0100

    Añadido patrón decorator

---
+++ Assets/Patrones Estructurales/Decorator/OrcoBaseSinArma.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Patrones_Estructurales.Decorator
+{
+    public class OrcoBaseSinArma : Component
+    {
+        //Otorgan implementaciones por defecto
+        //Pueden haber varios concrete components => OrcosBase, ElfosBase
+        public override int DamageDealt() => 10;
+    }
+}
---
+++ Assets/Patrones Estructurales/Decorator/PinchosParaMaza.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Patrones_Estructurales.Decorator
+{
+    public class PinchosParaMaza: Decorator
+    {
+        //Llaman al objeto envuelto y alteran su resultado
+        public PinchosParaMaza(Component comp) : base(comp) => Component = comp;
+
+        //Pueden implementar o no esta funcion, permitiendo su extension
+        public override int DamageDealt() => Component.DamageDealt() + 50;
+    }
+}
