93bbf3c2fa1fcdbf3889d45248030bb5f866145f
Toni <tperez@ggtech.es>
2022-12-15 13:16:44 +0100

    Flyweight method

---
+++ Assets/Patrones Estructurales/Flyweight/Flyweight.cs
@@ -0,0 +1,21 @@
+﻿using Unity.Plastic.Newtonsoft.Json;
+
+namespace Patrones_Estructurales.Flyweight
+{
+    //Almacena una parte común a todos los objetos
+    public class Flyweight
+    {
+        private Car _sharedState;
+
+        public Flyweight(Car sharedState)
+        {
+            _sharedState = sharedState;
+        }
+
+        public void Operation(Car uniqueState)
+        {
+            string s = JsonConvert.SerializeObject(_sharedState);
+            string u = JsonConvert.SerializeObject(uniqueState);
+        }
+    }
+}
