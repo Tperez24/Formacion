0c851b16486350eb233f6f55b7a96ed17a8254ef
Toni <tperez@ggtech.es>
2022-12-20 13:16:35 +0100

    no message

---
+++ Assets/PatronesDeComportamiento/State/MovementInput.cs
@@ -0,0 +1,22 @@
+﻿
+using UnityEngine.InputSystem.LowLevel;
+
+namespace PatronesDeComportamiento.State
+{
+    //Define la interfaz a los clientes, mantiene una referencia de la subclase que representa el estado del contexto
+    public class MovementInput
+    {
+        private MovementInputState _inputState;
+
+        public MovementInput(MovementInputState state) => TransitionTo(state);
+
+        public void TransitionTo(MovementInputState state)
+        {
+            _inputState = state;
+            _inputState.SetContext(this);
+        }
+
+        public void StateAction() => _inputState.Move();
+        public void StateTransition() => _inputState.Transition();
+    }
+}
---
+++ Assets/PatronesDeComportamiento/State/MovementInputState.cs
@@ -0,0 +1,10 @@
+﻿namespace PatronesDeComportamiento.State
+{
+    public abstract class MovementInputState
+    {
+        protected MovementInput MovementInput;
+        public void SetContext(MovementInput movementInput) => MovementInput = movementInput;
+        public abstract void Move();
+        public abstract void Transition();
+    }
+}
