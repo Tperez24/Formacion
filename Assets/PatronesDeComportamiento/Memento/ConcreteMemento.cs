7061f3bd092c311cce552247677b0d153668e711
Toni <tperez@ggtech.es>
2022-12-21 14:30:43 +0100

    no message

---
+++ Assets/PatronesDeComportamiento/Memento/ConcreteMemento.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PatronesDeComportamiento.Memento
+{
+    //Contiene la infraestructura para almacenar el estado del originador
+    public class ConcreteMemento : IMemento
+    {
+        private readonly string _state;
+        private readonly DateTime _dateTime;
+
+        public ConcreteMemento(string state)
+        {
+            _state = state;
+            _dateTime = DateTime.Now;
+        }
+
+        //Usado por el originador para restaurar
+        public string GetState() => _state;
+        
+        //El resto es usado por el careTaker para displayear la info
+        public string GetName() => _dateTime + " / " + _state;
+
+        public DateTime GetDate() => _dateTime;
+    }
+}
---
+++ Assets/PatronesDeComportamiento/Memento/IMemento.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PatronesDeComportamiento.Memento
+{
+    //Contiene la forma de recivir la data del concrete memento. sin exponer el estadod el originador
+    public interface IMemento
+    {
+        string GetName();
+        string GetState();
+        DateTime GetDate();
+    }
+}
